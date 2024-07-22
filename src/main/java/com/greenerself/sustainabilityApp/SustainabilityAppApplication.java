fd0d392f77f0ec3c8c8d252ced3d5690b8f2d265
asxd-10 <katreashay@gmail.com>
2024-07-22 16:26:11 +0530

    First commit - Setup Framework for Project

---
+++ src/main/java/com/greenerself/sustainabilityApp/SustainabilityAppApplication.java
@@ -0,0 +1,13 @@
+package com.greenerself.sustainabilityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustainabilityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustainabilityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenerself/sustainabilityApp/SustainabilityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenerself.sustainabilityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustainabilityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
